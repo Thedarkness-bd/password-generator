75c39a565582b538d5d61191042069a83a91394f
Thedarkness-bd <toisiftasrik@gmail.com>
2023-12-10 00:44:37 +0600

    Files added and some work done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="w-full bg-dark vh-100 text-white">
+      <h1 className="text-center pt-2">Password Generator</h1>
+    </div>
+  );
+}
+
+export default App;
